28b8ea289ca6d4adf394a263c6e680ea41a61f75
Javier Tibi <javier@digitalplankton.co>
2020-12-07 10:43:21 -0500

    Add images

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
 
 Route::get('/tweetMedia', function()
 {
-    $n = rand(1,26);
+    $n = rand(1,200);
     $path = resource_path() . '/images/'. $n.'.jpg';
 
     if(!File::exists($path)) {
