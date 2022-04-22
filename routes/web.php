02d44ac8f884f43957c0ab97d70f94717ec344ed
Javier Tibi <javier@digitalplankton.co>
2022-04-22 00:02:58 -0400

    Add pictures

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
 
 Route::get('/tweetMedia', function()
 {
-    $n = rand(1,1001);
+    $n = rand(1,1050);
     $path = resource_path() . '/images/'. $n.'.jpg';
 
     if(!File::exists($path)) {
